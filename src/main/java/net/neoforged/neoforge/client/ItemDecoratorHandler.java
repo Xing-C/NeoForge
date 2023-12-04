476d92c6fb56a7de08d4701fcf8227407c5ff890
Xing-C <79847143@qq.com>
2023-12-04 12:33:32 +0800

    Revert "[1.20.2] Fix blending issues caused by item decorators (#241)"
    This reverts commit c0f6ec7739f273e37e11e3e69110b152170748f3.

--- src/main/java/net/neoforged/neoforge/client/ItemDecoratorHandler.java
+++ src/main/java/net/neoforged/neoforge/client/ItemDecoratorHandler.java
@@ -22,7 +22,6 @@ import org.jetbrains.annotations.ApiStatus;
 @ApiStatus.Internal
 public final class ItemDecoratorHandler {
     private final List<IItemDecorator> itemDecorators;
-    private final GlStateBackup stateBackup = new GlStateBackup();
 
     private static Map<Item, ItemDecoratorHandler> DECORATOR_LOOKUP = ImmutableMap.of();
 
@@ -50,15 +49,11 @@ public final class ItemDecoratorHandler {
     }
 
     public void render(GuiGraphics guiGraphics, Font font, ItemStack stack, int xOffset, int yOffset) {
-        RenderSystem.backupGlState(stateBackup);
-
         resetRenderState();
         for (IItemDecorator itemDecorator : itemDecorators) {
             if (itemDecorator.render(guiGraphics, font, stack, xOffset, yOffset))
                 resetRenderState();
         }
-
-        RenderSystem.restoreGlState(stateBackup);
     }
 
     private void resetRenderState() {
