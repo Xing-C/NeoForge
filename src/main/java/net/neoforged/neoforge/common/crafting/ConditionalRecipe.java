7fd18f5e93c96ff32a2233cd52704857f016f328
Xing-C <79847143@qq.com>
2023-12-04 12:32:19 +0800

    Revert "Remove the `RecipeSerializer` patch (#313)"
    This reverts commit 892f48ffd0842b5ac1b4128bac0f0e3b0ded1ef9.

--- src/main/java/net/neoforged/neoforge/common/crafting/ConditionalRecipe.java
+++ src/main/java/net/neoforged/neoforge/common/crafting/ConditionalRecipe.java
@@ -13,6 +13,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.jetbrains.annotations.Nullable;
 
 public class ConditionalRecipe<T extends Recipe<?>> implements RecipeSerializer<T> {
     public static final Codec<Recipe<?>> CONDITIONAL_RECIPES_CODEC = RecipeManager.CONDITIONAL_DISPATCH.listOf().fieldOf("recipes")
@@ -26,8 +27,8 @@ public class ConditionalRecipe<T extends Recipe<?>> implements RecipeSerializer<
 
     // Should never get here as it's a wrapper
     @Override
-    public T fromNetwork(FriendlyByteBuf p_44106_) {
-        throw new UnsupportedOperationException("Attempted to write conditional recipe to network; this is a wrapper class!");
+    public @Nullable T fromNetwork(FriendlyByteBuf p_44106_) {
+        return null;
     }
 
     @Override
