b37982872865bc898b2a03f6df3e684103c9bfbc
Xing-C <79847143@qq.com>
2023-12-04 12:32:28 +0800

    Revert "Fix and change chunk ticket registration (#286)"
    This reverts commit 88bcdfbc60a6994bb536134a1f563f225c7f6963.

--- tests/src/main/java/net/neoforged/neoforge/debug/world/ForgeChunkManagerTest.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/world/ForgeChunkManagerTest.java
@@ -5,10 +5,11 @@
 
 package net.neoforged.neoforge.debug.world;
 
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.longs.LongSet;
 import java.util.Map;
 import java.util.UUID;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -20,10 +21,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.neoforge.common.world.chunk.RegisterTicketControllersEvent;
-import net.neoforged.neoforge.common.world.chunk.TicketController;
-import net.neoforged.neoforge.common.world.chunk.TicketSet;
+import net.neoforged.neoforge.common.world.ForcedChunkManager;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -39,29 +39,10 @@ public class ForgeChunkManagerTest {
     private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
     private static final DeferredBlock<Block> CHUNK_LOADER_BLOCK = BLOCKS.register("chunk_loader", () -> new ChunkLoaderBlock(Properties.of().mapColor(MapColor.STONE)));
     private static final DeferredItem<BlockItem> CHUNK_LOADER_ITEM = ITEMS.registerSimpleBlockItem(CHUNK_LOADER_BLOCK);
-    private static final TicketController CONTROLLER = new TicketController(new ResourceLocation(MODID, "default"), (world, ticketHelper) -> {
-        for (Map.Entry<BlockPos, TicketSet> entry : ticketHelper.getBlockTickets().entrySet()) {
-            BlockPos key = entry.getKey();
-            int ticketCount = entry.getValue().nonTicking().size();
-            int tickingTicketCount = entry.getValue().ticking().size();
-            if (world.getBlockState(key).is(CHUNK_LOADER_BLOCK.get()))
-                LOGGER.info("Allowing {} chunk tickets and {} ticking chunk tickets to be reinstated for position: {}.", ticketCount, tickingTicketCount, key);
-            else {
-                ticketHelper.removeAllTickets(key);
-                LOGGER.info("Removing {} chunk tickets and {} ticking chunk tickets for no longer valid position: {}.", ticketCount, tickingTicketCount, key);
-            }
-        }
-        for (Map.Entry<UUID, TicketSet> entry : ticketHelper.getEntityTickets().entrySet()) {
-            UUID key = entry.getKey();
-            int ticketCount = entry.getValue().nonTicking().size();
-            int tickingTicketCount = entry.getValue().ticking().size();
-            LOGGER.info("Allowing {} chunk tickets and {} ticking chunk tickets to be reinstated for entity: {}.", ticketCount, tickingTicketCount, key);
-        }
-    });
 
     public ForgeChunkManagerTest() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::registerControllers);
+        modEventBus.addListener(this::commonSetup);
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         modEventBus.addListener(this::addCreative);
@@ -72,8 +53,26 @@ public class ForgeChunkManagerTest {
             event.accept(CHUNK_LOADER_ITEM);
     }
 
-    private void registerControllers(RegisterTicketControllersEvent event) {
-        event.register(CONTROLLER);
+    private void commonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> ForcedChunkManager.setForcedChunkLoadingCallback(MODID, (world, ticketHelper) -> {
+            for (Map.Entry<BlockPos, Pair<LongSet, LongSet>> entry : ticketHelper.getBlockTickets().entrySet()) {
+                BlockPos key = entry.getKey();
+                int ticketCount = entry.getValue().getFirst().size();
+                int tickingTicketCount = entry.getValue().getSecond().size();
+                if (world.getBlockState(key).is(CHUNK_LOADER_BLOCK.get()))
+                    LOGGER.info("Allowing {} chunk tickets and {} ticking chunk tickets to be reinstated for position: {}.", ticketCount, tickingTicketCount, key);
+                else {
+                    ticketHelper.removeAllTickets(key);
+                    LOGGER.info("Removing {} chunk tickets and {} ticking chunk tickets for no longer valid position: {}.", ticketCount, tickingTicketCount, key);
+                }
+            }
+            for (Map.Entry<UUID, Pair<LongSet, LongSet>> entry : ticketHelper.getEntityTickets().entrySet()) {
+                UUID key = entry.getKey();
+                int ticketCount = entry.getValue().getFirst().size();
+                int tickingTicketCount = entry.getValue().getSecond().size();
+                LOGGER.info("Allowing {} chunk tickets and {} ticking chunk tickets to be reinstated for entity: {}.", ticketCount, tickingTicketCount, key);
+            }
+        }));
     }
 
     private static class ChunkLoaderBlock extends Block {
@@ -87,7 +86,7 @@ public class ForgeChunkManagerTest {
             super.onPlace(state, worldIn, pos, oldState, isMoving);
             if (worldIn instanceof ServerLevel) {
                 ChunkPos chunkPos = new ChunkPos(pos);
-                CONTROLLER.forceChunk((ServerLevel) worldIn, pos, chunkPos.x, chunkPos.z, true, true);
+                ForcedChunkManager.forceChunk((ServerLevel) worldIn, MODID, pos, chunkPos.x, chunkPos.z, true, false);
             }
         }
 
@@ -96,7 +95,7 @@ public class ForgeChunkManagerTest {
             super.onRemove(state, worldIn, pos, newState, isMoving);
             if (worldIn instanceof ServerLevel && !state.is(newState.getBlock())) {
                 ChunkPos chunkPos = new ChunkPos(pos);
-                CONTROLLER.forceChunk((ServerLevel) worldIn, pos, chunkPos.x, chunkPos.z, false, true);
+                ForcedChunkManager.forceChunk((ServerLevel) worldIn, MODID, pos, chunkPos.x, chunkPos.z, false, false);
             }
         }
     }
