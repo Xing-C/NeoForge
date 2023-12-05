7f2b60809a37b97975f78426c37b201d81c59104
Xing-C <79847143@qq.com>
2023-12-05 23:16:35 +0800

    fix

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/LauncherJson.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/LauncherJson.groovy
@@ -8,6 +8,8 @@ import org.gradle.api.provider.SetProperty
 import org.gradle.api.tasks.*
 
 import java.nio.file.Files
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 import static net.minecraftforge.forge.tasks.Util.getArtifacts
 import static net.minecraftforge.forge.tasks.Util.iso8601Now
@@ -19,7 +21,8 @@ abstract class LauncherJson extends DefaultTask {
 
     @Internal final vanilla = project.project(':mcp').file('build/mcp/downloadJson/version.json')
     @Internal final timestamp = iso8601Now()
-    @Internal final id = "${project.rootProject.ext.MC_VERSION}-${project.name}${project.version.substring(project.rootProject.ext.MC_VERSION.length())}"
+    //@Internal final id = "${project.rootProject.ext.MC_VERSION}-${project.name}${project.version.substring(project.rootProject.ext.MC_VERSION.length())}"
+    @Internal final id = "${project.rootProject.ext.VERSION}"
 
     LauncherJson() {
         getOutput().convention(project.layout.buildDirectory.file('version.json'))
@@ -40,6 +43,24 @@ abstract class LauncherJson extends DefaultTask {
         getArtifacts(project, project.configurations.pluginLayerLibrary, false).each { key, lib -> libs[key] = lib }
 
         libs.each { key, lib -> json.libraries.add(lib) }
+
         Files.writeString(output.get().asFile.toPath(), new JsonBuilder(json).toPrettyString())
+
+        String json = Files.readString(output.get().asFile.toPath());
+        // 定义要查找和替换的正则表达式
+        String regex = "https://maven.neoforged.net/releases/net/neoforged/fancymodloader";
+        String replacement = "https://p.mcxkly.cn/d/1/fancymodloader";
+
+        // 创建Pattern对象
+        Pattern pattern = Pattern.compile(regex);
+
+        // 创建Matcher对象，并将其应用于JSON文本
+        Matcher matcher = pattern.matcher(json);
+
+        // 使用Matcher的replaceAll方法替换匹配到的内容
+        json = matcher.replaceAll(replacement);
+
+        // 将修改后的JSON文本保存到文件
+        Files.writeString(output.get().asFile.toPath(), json);
     }
 }
